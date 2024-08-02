300e55426d2387af98f8f9a7ff6f1a17bfbc6e75
irvingdevjava <irvingspringboot@gmail.com>
2024-08-01 21:41:51 -0600

    Implementé PlayerRepositoryImpl con métodos CRUD
    - Añadí implementación de los métodos findAll, findById, save y deleteById
    - Los métodos están marcados como TODO para implementación futura

---
+++ src/main/java/org/mock/persistence/repository/PlayerRepositoryImpl.java
@@ -0,0 +1,33 @@
+package org.mock.persistence.repository;
+
+import java.util.List;
+
+import org.mock.persistence.entity.Player;
+
+public class PlayerRepositoryImpl implements IPlayerRepository{
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<Player> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Player findById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void save(Player player) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
