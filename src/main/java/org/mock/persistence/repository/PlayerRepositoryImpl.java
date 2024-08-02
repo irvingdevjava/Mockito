3c57ad5c289cf70d33c2a279f6808bfbed8c3e36
irvingdevjava <irvingspringboot@gmail.com>
2024-08-02 11:42:09 -0600

    Implementé métodos reales en PlayerRepositoryImpl
    - Añadí una base de datos en memoria con una lista de jugadores
    - Implementé los métodos findAll, findById, save y deleteById con lógica real
    - Agregué mensajes de impresión para cada método para indicar su ejecución

--- src/main/java/org/mock/persistence/repository/PlayerRepositoryImpl.java
+++ src/main/java/org/mock/persistence/repository/PlayerRepositoryImpl.java
@@ -1,33 +1,49 @@
 package org.mock.persistence.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.mock.persistence.entity.Player;
 
 public class PlayerRepositoryImpl implements IPlayerRepository{
 
+    private List<Player> playerDatabase = new ArrayList<>(List.of(
+           new Player(1L, "Lionel Messi", "Inter Miami", "Delantero"),
+           new Player(2L, "Cristiano Ronaldo", "Al Nassr", "Delantero"),
+           new Player(3L, "Neymar Jr.", "Paris Saint-Germain", "Delantero"),
+           new Player(4L, "Kylian Mbappé", "Paris Saint-Germain", "Delantero"),
+           new Player(5L, "Kevin De Bruyne", "Manchester City", "Volante"),
+           new Player(6L, "Virgil van Dijk", "Liverpool", "Defensa")
+   ));
+
     @Override
     public void deleteById(Long id) {
-        // TODO Auto-generated method stub
+        System.out.println("--> Metodo deleteById real");
+        this.playerDatabase= this.playerDatabase.stream()
+                                .filter(player -> player.getId() != id)
+                                .toList() ;
         
     }
 
     @Override
     public List<Player> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        System.out.println("--> Metodo findAll real");
+        return this.findAll();
     }
 
     @Override
     public Player findById(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+        System.out.println("--> Metodo findById real");
+        return this.playerDatabase.stream()
+                    .filter(player -> player.getId()== id)
+                    .findFirst()
+                    .orElseThrow();
     }
 
     @Override
     public void save(Player player) {
-        // TODO Auto-generated method stub
-        
+        System.out.println("--> Metodo save real");
+        this.playerDatabase.add(player);
     }
 
 }
